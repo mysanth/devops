906ac5ebf483cc02bc10661a3c385a8996d85022
mysanth_07 <mysanthp7@gmail.com>
2025-06-26 18:54:52 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "mysanthp-devops-starter-${terraform.random_id.suffix.hex}"
+  bucket = "mysanthp-devops-${terraform.random_id.suffix.hex}"
 }
 
 resource "random_id" "suffix" {
